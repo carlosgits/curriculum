a1a60996a152a87866e7d770f31254bd864c26e9
carlosgits <122765151+carlosgits@users.noreply.github.com>
2023-08-17 00:04:33 -0400

    Align trade now button in the cripto cards

--- Landing-pages/cryptopage/src/components/cards.jsx
+++ Landing-pages/cryptopage/src/components/cards.jsx
@@ -1,37 +1,44 @@
 import "../styles/cards.css"
 
-const Cards = ({width}) =>{
+const Cards = ({ width }) => {
 
-return(            
+    return (
 
-<div className="card-container">
-<div className="card" style={{width: width}}>
-    <img class="card-img-top" src={process.env.PUBLIC_URL + '/bitcoinImg.svg'} alt="Card image cap2"/>
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-</div>
+        <div className="card-deck card-container">
+            <div className="card first-card" style={{ width: width }}>
+                <img class="card-img-top" src={process.env.PUBLIC_URL + '/bitcoinImg.svg'} alt="bitcoin card info" />
+                <div class="card-body">
+                    <h5 class="card-title">Bitcoin</h5>
+                    <p class="card-text">
+                        As the pioneer, Bitcoin securely transfers value globally. With a capped supply of 21M coins, it reshapes finance.</p>
+                    <div className="btn-container">
+                        <a href="#" class="btn  btn-cont">TRADE NOW</a>
+                    </div>
+                </div>
+            </div>
 
-<div className="card" style={{width: width}}>
-    <img class="card-img-top" src={process.env.PUBLIC_URL + '/litecoinImg.svg'} alt="Card image cap1"/>
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-</div>
+            <div className="card" style={{ width: width }}>
+                <img class="card-img-top" src={process.env.PUBLIC_URL + '/litecoinImg.svg'} alt="Litecoin card info" />
+                <div class="card-body">
+                    <h5 class="card-title">Litecoin</h5>
+                    <p class="card-text">Known as digital silver, Litecoin offers swift transactions. With a total of 84M coins, it's practical for daily use.</p>
+                    <div className="btn-container">
+                        <a href="#" class="btn btn-cont text-right">TRADE NOW</a>
+                    </div>
+                </div>
+            </div>
 
-<div className="card" style={{width: width}}>
-    <img class="card-img-top" src={process.env.PUBLIC_URL + '/ethereumImg.svg'} alt="Card image cap3"/>
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-</div>
-</div>)
+            <div className="card last-card" style={{ width: width }}>
+                <img class="card-img-top" src={process.env.PUBLIC_URL + '/ethereumImg.svg'} alt="Ethereum card info" />
+                <div class="card-body">
+                    <h5 class="card-title">Ethereum</h5>
+                    <p class="card-text">Beyond cryptocurrency, Ethereum empowers smart contracts and dApps. Ether drives innovation across diverse industries.</p>
+                    <div className="btn-container">
+                        <a href="#" class="btn btn-cont">TRADE NOW</a>
+                    </div>
+                </div>
+            </div>
+        </div>)
 
 }
 
