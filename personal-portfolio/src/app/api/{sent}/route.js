e7c0db4c0c05a2ea9c78058b661f0053b21e89cc
carlosgits <122765151+carlosgits@users.noreply.github.com>
2023-12-05 07:56:11 -0400

    personal-portfolio-fix

--- personal-portfolio/src/app/api/{sent}/route.js
+++
@@ -1,7 +0,0 @@
-import { NextServer } from "next/dist/server/next";
-
-
-export default function handler(req, res) {
-  
-  
-}
--- personal-portfolio/tailwind.config.js
+++ personal-portfolio/tailwind.config.js
@@ -4,25 +4,20 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ], plugins: [
-    require("flowbite/plugin")
   ],
   theme: {
     colors: {
       "portfolio-green": "#4EE1A0",
       "portfolio-gray": "#d9d9d9",
       "portfolio-darkgray": "#242424",
-      "portfolio-tomato": "#FF6F5B",
-      
+      "portfolio-tomato": "#FF6F5B",    
     },
-
     fontSize: {
       normal: ["18px", "28px"],
       XL: ["88px", "88px"],
       L: ["48px", "56px"],
       M: ["24px", "32px"],
     },
-
     fontFamily: {
       Grotesk: ["Grotesk", "sans-serif"],
     },
@@ -30,8 +25,7 @@ module.exports = {
   extend: {
     backgroundImage: {
       "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      "gradient-conic":
-        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
     },
   },
   plugins: [],
